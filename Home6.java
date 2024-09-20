2bf2f183b7f3ce40e7c54621319c571b56052cb8
chobotaraleks <79744088+chobotaraleks@users.noreply.github.com>
2024-09-21 02:37:35 +0300

    Update Home6.java

--- Home6.java
+++ Home6.java
@@ -1,1 +1,19 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0; // змінна для накопичення суми
+
+        // Цикл від 1 до 6, включно
+        for (int i = 1; i <= 6; i++) {
+            sum += i; // додати поточне число до суми
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
+        }
+
+        // Виведення підсумкової суми
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
+    }
+}
 
